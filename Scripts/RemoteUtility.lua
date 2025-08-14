1966f1b5a80c9cee1dcd3d56168966ece68fe8c9
Sai <133896936+sqistqr@users.noreply.github.com>
2025-08-14 11:43:28 -0500

    Update RemoteUtility.lua

--- Scripts/RemoteUtility.lua
+++ Scripts/RemoteUtility.lua
@@ -1,5 +1,6 @@
 --[[
-MS1 REMOTE UTILITY v1.1
+MS1 REMOTE UTILITY v1.2
+v1.2: added loadstring settings thingy..
 What MS1 Remote Utility is & isnt:
 + It prevents remote bans for MS1
 + Puts the remote in the global enviroment, variable name is Remote (can be changed)
@@ -10,84 +11,97 @@ What MS1 Remote Utility is & isnt:
 by Sai<3
 ]]
 
---Customization, if you don't know what these do, don't mess with them!
---Loaded prompt (i HIGHLY reccomend keeping this enabled.)
-local loadedPrompt = true
---Global remote
-local globalRemote = true
-local globalRemoteName = "Remote"
---Debug/Reports
-local reportRedirects = true
-
---SCRIPT BELOW! DON'T MESS WITH IT IF YOU DON'T KNOW WHAT YOU'RE DOING. ANY ISSUES THAT OCCUR FROM YOU MESSING WITH THIS CODE BELOW IS NOT MY FAULT.
 --Making sure the game is Mining Simulator just incase this is in your autoexecute
 if game.PlaceId ~= 1417427737 then
 	return
 end
 
---For security purposes.
+--Some executors don't have script support (somehow) I think
+local script = Instance.new("Script")
 
---Defining variables
-local hookmetamethod, Remote, Verified1, Verified2, VerifyHook, RemoteHook1, RemoteHook2, RemoteHook3 = hookmetamethod, game:GetService("ReplicatedStorage").Network:InvokeServer()
+--Script settings lol
+local ScriptSettings = {
+	--Loaded prompt (i HIGHLY reccomend keeping this enabled.)
+	loadedPrompt = true,
+	
+	--Global remote
+	globalRemote = true,
+	globalRemoteName = "Remote",
+	
+	--Debug/Reports
+	reportRedirects = true
+}
 
---Making sure the Remote is real (by running if x then it will check if it is nil or not in this case, some executors may fake UNC functions leading this to be nil.)
-if Remote then
-	Verified1 = true
-end
-
---Making sure the executor supports hookmetamethod
-VerifyHook = hookmetamethod(game, "__namecall", function(Self, ...)
-    if getnamecallmethod():lower() == "verify" then
-    	Verified2 = true
-    else
-  	  return VerifyHook(Self, ...)
-    end
-end)
-game:Verify()
-
---Checking if verification passed, if not kicking the player so they don't ban themselves xd
-if Verify1 == false or Verify2 == false then
-	game:GetService("Players").LocalPlayer:Kick("Sorry, your executor can't run this script.")
-end
-
---Defining global Remote so you can simply type Remote
-if globalRemote == true then
-	getgenv()[globalRemoteName] = Remote
-end
-
---Making WaitForChild return the remote
-RemoteHook1 = hookmetamethod(game, "__namecall", function(Self, ...)
-    if getnamecallmethod():lower() == "waitforchild" and (...) == "ř̷͔̗̘̗̱̀ė̶͓̿̿̈́̎̌͛͊̅̄̕͘͝m̷̯̬̬̤̥̜̲͂͐̈̄̊̿̓̈́̍̌́ŏ̵̊̓̆̈́" then
-    	return Remote
-    end
-    return RemoteHook1(Self, ...)
-end)
-
---If you try to get remo via workspace[""]
-local RemoteHook2
-RemoteHook2 = hookmetamethod(game, "__index", function(Self, Method)
-    if Method:lower() == "ř̷͔̗̘̗̱̀ė̶͓̿̿̈́̎̌͛͊̅̄̕͘͝m̷̯̬̬̤̥̜̲͂͐̈̄̊̿̓̈́̍̌́ŏ̵̊̓̆̈́" then
-        return Remote
-    end  
-    return RemoteHook2(Self, Method)
-end)
-
---Anti ban & remote redirection for stuff like remote spy :)
-RemoteHook3 = hookmetamethod(game, "__namecall", function(Self, ...)
-	if getnamecallmethod():lower() == "fireserver" and Self ~= Remote then
-		if reportRedirects == true then
-			warn("Redirected "..Self.Name.." (fake remote) to the correct remote.")
+--SCRIPT BELOW! DON'T MESS WITH IT IF YOU DON'T KNOW WHAT YOU'RE DOING. ANY ISSUES THAT OCCUR FROM YOU MESSING WITH THIS CODE BELOW IS NOT MY FAULT.
+return function(Settings)
+	for Setting,Value in Settings do
+		print(Setting,Value)
+		if ScriptSettings[Setting] then
+			ScriptSettings[Setting] = Value 
 		end
-		return RemoteHook3(Remote, ...)
 	end
-	return RemoteHook3(Self, ...)
-end)
+	
+	--Defining variables
+	local hookmetamethod, Remote, Verified1, Verified2, VerifyHook, RemoteHook1, RemoteHook2, RemoteHook3 = hookmetamethod, game:GetService("ReplicatedStorage").Network:InvokeServer()
 
-if loadedPrompt == true then
-	game:GetService("StarterGui"):SetCore("SendNotification", {
-		Title = "Remote Utilities v1.1\nLoaded!",
-		Text = "\nby Sai<3 (@.sqi.stqr. for help!)",
-		Duration = math.huge,
-		Button1 = "Got it!"
-	})
+	--Making sure the Remote is real (by running if x then it will check if it is nil or not in this case, some executors may fake UNC functions leading this to be nil.)
+	if Remote then
+		Verified1 = true
+	end
+
+	--Making sure the executor supports hookmetamethod
+	VerifyHook = hookmetamethod(game, "__namecall", function(Self, ...)
+	    if getnamecallmethod():lower() == "verify" then
+ 	   	Verified2 = true
+ 	   else
+	  	  return VerifyHook(Self, ...)
+	    end
+	end)
+	script:Verify()
+
+	--Checking if verification passed, if not kicking the player so they don't ban themselves xd
+	if Verify1 == false or Verify2 == false then
+		game:GetService("Players").LocalPlayer:Kick("Sorry, your executor can't run this script.")
+	end
+
+	--Defining global Remote so you can simply type Remote
+	if ScriptSettings.globalRemote == true then
+		getgenv()[ScriptSettings.globalRemoteName] = Remote
+	end
+
+	--Making WaitForChild return the remote
+	RemoteHook1 = hookmetamethod(game, "__namecall", function(Self, ...)
+	    if getnamecallmethod():lower() == "waitforchild" and (...) == "ř̷͔̗̘̗̱̀ė̶͓̿̿̈́̎̌͛͊̅̄̕͘͝m̷̯̬̬̤̥̜̲͂͐̈̄̊̿̓̈́̍̌́ŏ̵̊̓̆̈́" then
+ 	   	return Remote
+ 	   end
+ 	   return RemoteHook1(Self, ...)
+	end)
+
+	--If you try to get remo via workspace[""]
+	RemoteHook2 = hookmetamethod(game, "__index", function(Self, Method)
+	    if Method:lower() == "ř̷͔̗̘̗̱̀ė̶͓̿̿̈́̎̌͛͊̅̄̕͘͝m̷̯̬̬̤̥̜̲͂͐̈̄̊̿̓̈́̍̌́ŏ̵̊̓̆̈́" then
+	        return Remote
+	    end  
+	    return RemoteHook2(Self, Method)
+	end)
+
+	--Anti ban & remote redirection for stuff like remote spy :)
+	RemoteHook3 = hookmetamethod(game, "__namecall", function(Self, ...)
+		if getnamecallmethod():lower() == "fireserver" and Self ~= Remote then
+			if ScriptSettings.reportRedirects == true then
+				warn("Redirected "..Self.Name.." (fake remote) to the correct remote.")
+			end
+			return RemoteHook3(Remote, ...)
+		end
+		return RemoteHook3(Self, ...)
+	end)
+
+	if ScriptSettings.loadedPrompt == true then
+		game:GetService("StarterGui"):SetCore("SendNotification", {
+			Title = "Remote Utilities v1.1\nLoaded!",
+			Text = "\nby Sai<3 (@.sqi.stqr. for help!)",
+			Duration = math.huge,
+			Button1 = "Got it!"
+		})
+	end
 end
